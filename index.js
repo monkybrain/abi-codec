f5249a47ba90b570be484c130d7e4b5d39d36545
Philip Prophet <philip.prophet@gmail.com>
2019-09-16 09:32:06 +0200

    Bumps web3.js version to 1.2.1

--- index.js
+++ index.js
@@ -1,40 +1,61 @@
-const { AbiCoder } = require('web3-eth-abi')
-
-const codec = new AbiCoder()
+const AbiCoder = require('web3-eth-abi')
 
 /*** PUBLIC ***/
 exports.encodeInput = (contractABI, method, params) => {
-  
+
   // Get method ABI
   const methodABI = getMethodABI(contractABI, method)
-  
+
   // Return encoded input
-  return codec.encodeFunctionCall(methodABI, params)
+  return AbiCoder.encodeFunctionCall(methodABI, params)
 }
 
 exports.decodeOutput = (contractABI, method, output) => {
-  
+
   // Get method ABI
-  const methodABI = getMethodABI(contractABI, method) 
-  
+  const methodABI = getMethodABI(contractABI, method)
+
   // Get, downcase and return output(s)
-  const outputs = codec.decodeParameters(methodABI.outputs, output)
+  const outputs = AbiCoder.decodeParameters(methodABI.outputs, output)
   return outputs.map( (output) => output.toLowerCase() )
-    
+
 }
 
 /*** PRIVATE ***/
 const getMethodABI = (contractABI, method) => {
-  
+
   // Find method by name
   const methodABI = contractABI.find(x => x.name === method)
-  
+
   // If not found -> throw error
   if (methodABI === undefined) {
     throw new Error(`Invalid method name: '${method}'`)
   }
-  
+
   // Return method ABI
   return methodABI
-  
+
 }
+
+jsonInterface = [
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "node",
+        "type": "bytes32"
+      }
+    ],
+    "name": "resolver",
+    "outputs": [
+      {
+        "name": "",
+        "type": "address"
+      }
+    ]
+  }
+]
+
+// Encode input for JSON RPC method `eth_call`
+const namehash = '0x78c5b99cf4668cf6da387866de4331c78b75b7db0087988c552f73e1714447b9' // 32 byte name hash for 'ethereum.eth'
+console.log(exports.encodeInput(jsonInterface, 'resolver', [namehash]))
